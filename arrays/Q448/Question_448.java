05435c4cb5a211bf46488a905482841653f8c227
Nidarsh N <nidarsh39@gmail.com>
2023-05-24 10:52:34 -0500

    Adding Java Solutions

---
+++ arrays/Q448/Question_448.java
@@ -0,0 +1,26 @@
+package arrays.Q448;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution{
+    public ArrayList<Integer> findDisappearedNumbers(ArrayList<Integer> nums){
+        return null;
+
+    }
+
+    public void grader(){
+
+    }
+}
+public class Question_448{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ArrayList<Integer> output1, output2;
+        ArrayList<Integer> input1 = new ArrayList<Integer>(Arrays.asList(4,3,2,7,8,2,3,1));
+        ArrayList<Integer> input2 = new ArrayList<Integer>(Arrays.asList(5,6));
+        output1 = solution.findDisappearedNumbers(input1);
+        output2 = solution.findDisappearedNumbers(input2);
+        
+    }
+}
